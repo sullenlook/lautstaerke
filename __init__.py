ac2b27213150ffc01f32e99b7b8924fe151cf534
sullenlook <sullenlook@sullenlook.eu>
2012-07-29 10:10:41 +0200

    upload

---
+++ __init__.py
@@ -0,0 +1,13 @@
+    @register("de-DE", "musik.*leiser")
+    def musikleiser(self, speech, language):
+        if language == 'de-DE':
+            value = self.ask(u"Auf was soll die Lautstärke geändert werden?")
+            os.system("amixer set Master \"{0}\" ".format(value))
+            self.say(u"Ok, ich habe die Lautstärke für dich auf \"{0}\" gesetzt.".format(value))
+
+    @register("de-DE", "musik.*lauter")
+    def musikleiser(self, speech, language):
+        if language == 'de-DE':
+            value = self.ask(u"Auf was soll die Lautstärke geändert werden?")
+            os.system("amixer set Master \"{0}\" ".format(value))
+            self.say(u"Ok, ich habe die Lautstärke für dich auf \"{0}\" gesetzt.".format(value))
